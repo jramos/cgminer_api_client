c6552bf00c8f1605f5f11bc9601ac2ad6cba3cb8
Justin Ramos <justin.ramos@gmail.com>
2014-07-24 18:26:37 -0500

    refactoring

--- lib/cgminer_api_client/pool.rb
+++ lib/cgminer_api_client/pool.rb
@@ -20,32 +20,5 @@ module CgminerApiClient
     def method_missing(name, *args)
       query(name, *args)
     end
-
-    def print_summary
-      @total_ghs = 0
-
-      @miners.each do |miner|
-        if miner.available?
-          summary = miner.summary[:summary][0]
-          devices = miner.devs[:devs]
-
-          @total_ghs += summary[:ghs_av]
-
-          puts "#{miner.host}:#{miner.port}"
-          puts "\tGH/s\t#{summary[:ghs_av]}"
-          puts "\tDevices\t#{devices.count}"
-
-          devices.each do |device|
-            puts "\t\tName\t#{device[:name]}"
-            puts "\t\tStatus\t#{device[:status]}"
-            puts "\t\tElapsed\t#{device[:device_elapsed] / 60} min"
-            puts "\t\tTemp\t#{device[:temperature]} C"
-            puts "\t\tErr\t#{device[:'device_hardware%']}%\n\n"
-          end
-        end
-      end
-
-      puts "\nTotal GH/s: #{@total_ghs.round(1)} (#{(@total_ghs / @miners.count).round(1)} avg/miner)"
-    end
   end
 end
